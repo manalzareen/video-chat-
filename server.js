d3480513ab6d51235f7874c286a45c8f3ea9cc75
Manal <manalzareen3108@gmail.com>
2024-04-03 17:36:14 +0530

    video app

--- server.js
+++ server.js
@@ -1,28 +1,6 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-
-
-app.set("view engine","ejs")
-app.use(express.static("public"))
-const {v4:uuidv4}  = require("uuid")
-
-const io = require("socket.io")(server, {
-    cors: {
-        origin: '*'
-    }
-});
-
-const { ExpressPeerServer } = require("peer");
-const peerServer = ExpressPeerServer(server, {
-    debug: true,
-});
-
-app.use("/peerjs", peerServer);
-var nodemailer = require('nodemailer');
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.json())
@@ -48,8 +26,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-        user: 'manalzareen3108@gm',
-        pass: 'sxcs lxsc nbdc ulhx',
+        user: 'manalzareen3108@gmail.com',
+        pass: 'sxcslxscnbdculhx',
     },
     secure: true,
 });
@@ -75,9 +53,7 @@ app.post("/send-mail", (req, res) => {
         if (error) {
             return console.log(error);
         }
-        res.status(200).send({ 
-            message: "Invitation sent!", 
-            message_id: info.messageId });
+        res.status(200).send({ message: "Invitation sent!", message_id: info.messageId });
     });
 })
 
@@ -92,56 +68,3 @@ io.on("connection", (socket) => {
 });
 
 server.listen(process.env.PORT || 3030);
-const transporter = nodemailer.createTransport({
-    port: 465,
-    host: "smtp.gmail.com",
-    auth: {
-        user: 'manalzareen3108@gmail.com',
-        pass: 'sxcs lxsc nbdc ulhx',
-    },
-    secure: true,
-});
-
-app.get("/" ,(req,res)  =>{
-    res.redirect(`/${uuidv4()}`)    
-})
-app.get("/:room",(req,res) =>{
-    res.render("index",{roomId:req.params.room})
-})
-
-app.post("/send-mail", (req, res) => {
-    const to = req.body.to;
-    const url = req.body.url;
-    const maildata = {
-        from: "manalzareen3108@gmail.com",
-        to: to,
-        subject: "video-chat with me",
-        html: `<p>Hey there,</p><p>Come and join me for a video chat here - ${url}</p>`
- 
-    } 
-    transporter.sendMail(mailData, (error, info) => {{
-        if(error){
-            return console.log(error)
-        }
-        res.status(200).send({
-            message:"invitation sent",
-            message_id: info.messageId
-        })
-    })
-})
-
-
-io.on("connection",(socket) => {
-
-    socket.on("join-room",(roomId,userId,userName) =>{
-        socket.join(roomId)
-        io.to(roomId).emit("user-connected", userId);
-        socket.on("message",(message) => {
-            io.to(roomId).emit("createMessage",message,userName)
-        })
-    })
-
-    
-})
-
-server.listen(3030);
